2c7f488c9bdb96c796c60e2da210a4be5baf4bec
zonble <zonble@gmail.com>
2024-07-16 14:41:28 +0800

    Beefs up OpenAiTtsEngine.

--- lib/src/imp/openai_tts_engine.dart
+++ lib/src/imp/openai_tts_engine.dart
@@ -7,12 +7,59 @@ import 'package:uuid/uuid.dart';
 
 import '../interface/tts_engine.dart';
 
+enum OpenAiTtsEngineModel {
+  tts1,
+  tts1Hd,
+}
+
+extension OpenAiTtsEngineModelToString on OpenAiTtsEngineModel {
+  String get stringRepresentation {
+    switch (this) {
+      case OpenAiTtsEngineModel.tts1:
+        return "tts-1";
+      case OpenAiTtsEngineModel.tts1Hd:
+        return "tts-1-hd";
+    }
+  }
+}
+
+enum OpenAiTtsEngineVoice {
+  alloy,
+  echo,
+  fable,
+  onyx,
+  nova,
+  shimmer,
+}
+
+extension OpenAiTtsEngineVoiceToString on OpenAiTtsEngineVoice {
+  String get stringRepresentation {
+    switch (this) {
+      case OpenAiTtsEngineVoice.alloy:
+        return "alloy";
+      case OpenAiTtsEngineVoice.echo:
+        return "echo";
+      case OpenAiTtsEngineVoice.fable:
+        return "fable";
+      case OpenAiTtsEngineVoice.onyx:
+        return "onyx";
+      case OpenAiTtsEngineVoice.nova:
+        return "nova";
+      case OpenAiTtsEngineVoice.shimmer:
+        return "shimmer";
+    }
+  }
+}
+
 /// An OpenAI API based TTS (Text-to-Speech) engine.
 ///
 /// Please note that the engine does not support Flutter Web, since it downloads
 /// audio to a temporary file but Flutter Web does not support storing temporary
 /// files.
 class OpenAiTtsEngine extends TtsEngine {
+  /// "tts-1" or "tts-1-hd".
+  ///
+  /// See also [OpenAiTtsEngineModel].
   final String model;
   final _player = AudioPlayer();
   var _rate = 1.0;
@@ -65,6 +112,8 @@ class OpenAiTtsEngine extends TtsEngine {
   /// - tts-1: alloy, echo, fable, onyx, nova, and shimmer (configurable)
   /// - tts-1-hd: alloy, echo, fable, onyx, nova, and shimmer (configurable,
   ///   uses OpenAI samples by default)
+  ///
+  /// See also [OpenAiTtsEngineVoice].
   @override
   Future<void> setVoice(Map<String, String> voice) async =>
       this._voice = voice.entries.first.value;
